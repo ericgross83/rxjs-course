78cfcf77b3d97259ef1ed3d85fd300d59af818af
Eric Groß <ericgross.eg@gmail.com>
2019-02-25 14:33:29 +0100

    Dit the stuff

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,11 +1,9 @@
 import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {Course} from '../model/course';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import {Store} from '../common/store.service';
 
 @Component({
     selector: 'course-dialog',
@@ -16,16 +14,17 @@ export class CourseDialogComponent implements AfterViewInit {
 
     form: FormGroup;
 
-    course:Course;
+    course: Course;
 
     @ViewChild('saveButton') saveButton: ElementRef;
 
-    @ViewChild('searchInput') searchInput : ElementRef;
+    @ViewChild('searchInput') searchInput: ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course: Course,
+        private store: Store) {
 
         this.course = course;
 
@@ -33,7 +32,7 @@ export class CourseDialogComponent implements AfterViewInit {
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
 
     }
@@ -51,4 +50,11 @@ export class CourseDialogComponent implements AfterViewInit {
     }
 
 
+  save() {
+    this.store.saveCourse(this.course.id, this.form.value)
+      .subscribe(
+        () => this.close(),
+        error => console.log('Error saving course', error)
+    );
+  }
 }
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,21 +1,23 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
+import {ActivatedRoute} from '@angular/router';
+import {Course} from '../model/course';
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
-    map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  map,
+  concatMap,
+  switchMap,
+  withLatestFrom,
+  concatAll, shareReplay, first
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {merge, fromEvent, Observable, concat, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {createHttpObservable} from '../common/util';
+import {Store} from '../common/store.service';
+import {store} from '@angular/core/src/render3';
 
 
 @Component({
@@ -25,16 +27,16 @@ import {createHttpObservable} from '../common/util';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-    courseId:string;
+    courseId: number;
 
-    course$ : Observable<Course>;
+    course$: Observable<Course>;
 
     lessons$: Observable<Lesson[]>;
 
 
     @ViewChild('searchInput') input: ElementRef;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private store: Store) {
 
 
     }
@@ -44,6 +46,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
         this.courseId = this.route.snapshot.params['id'];
 
         this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = this.store.selectCourseById(this.courseId)
+          .pipe(
+            first()
+          );
+
+        forkJoin(this.course$, this.loadLessons())
+          .subscribe(console.log);
 
     }
 
@@ -67,7 +76,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
         return createHttpObservable(
             `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
             .pipe(
-                map(res => res["payload"])
+                map(res => res['payload'])
             );
     }
 
